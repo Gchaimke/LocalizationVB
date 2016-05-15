4d67b06351850fa6568ddc669c9cd492f1a53eec
gchaim <gchaim@TALAM-LT>
2016-05-15 11:26:09 +0300

    start

---
+++ multylang/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        resources.ApplyResources(Me.ComboBox1, "ComboBox1")
+        Me.ComboBox1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.multylang.My.MySettings.Default, "language", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {resources.GetString("ComboBox1.Items"), resources.GetString("ComboBox1.Items1")})
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Text = Global.multylang.My.MySettings.Default.language
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ multylang/Form1.vb
@@ -0,0 +1,42 @@
+﻿
+Imports System.Globalization
+Imports System.ComponentModel
+Public Class Form1
+    Private Property CultureInfo As CultureInfo
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        My.Settings.language = ComboBox1.Text
+
+        My.Settings.Save()
+
+        If ComboBox1.SelectedIndex = 0 Then
+
+            ChangeLanguage("en")
+
+        Else
+
+            ChangeLanguage("he-IL")
+
+        End If
+
+
+    End Sub
+
+
+    Private Sub ChangeLanguage(ByVal Language As String)
+
+        For Each c As Control In Me.Controls
+
+            Dim crmLang As ComponentResourceManager = New ComponentResourceManager(GetType(Form1))
+
+            crmLang.ApplyResources(c, c.Name, New CultureInfo(Language)) 'Set desired language
+
+        Next c
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ChangeLanguage(My.Settings.language)
+    End Sub
+End Class
