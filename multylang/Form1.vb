d68131d963a04d9a88d8b72b859dc371fad8c52a
gchaim <gchaim@TALAM-LT>
2016-05-23 14:07:33 +0300

    Resources

--- multylang/Form1.vb
+++ multylang/Form1.vb
@@ -1,8 +1,13 @@
 ﻿
 Imports System.Globalization
 Imports System.ComponentModel
+Imports System.Resources
+Imports System.Reflection
+Imports System.Threading
 Public Class Form1
     Private Property CultureInfo As CultureInfo
+    Dim resFile As String = "multylang.MsgRes"
+    Dim resources As New ResourceManager(resFile, [Assembly].GetExecutingAssembly())
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
@@ -13,11 +18,11 @@ Public Class Form1
         If ComboBox1.SelectedIndex = 0 Then
 
             ChangeLanguage("en")
-
+            MessageBox.Show(resources.GetString("MyStr"))
         Else
 
             ChangeLanguage("he-IL")
-
+            MessageBox.Show(resources.GetString("MyStr"))
         End If
 
 
@@ -33,10 +38,12 @@ Public Class Form1
             crmLang.ApplyResources(c, c.Name, New CultureInfo(Language)) 'Set desired language
 
         Next c
-
+        Thread.CurrentThread.CurrentUICulture = New CultureInfo(My.Settings.language)
+        Thread.CurrentThread.CurrentCulture = New CultureInfo(My.Settings.language)
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ChangeLanguage(My.Settings.language)
+
     End Sub
 End Class
--- multylang/MsgRes.Designer.vb
+++ multylang/MsgRes.Designer.vb
@@ -24,22 +24,26 @@ Namespace My.Resources
     '''</summary>
     <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"),  _
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.Microsoft.VisualBasic.HideModuleNameAttribute()>  _
-    Friend Module Resources
+     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
+    Friend Class MsgRes
         
-        Private resourceMan As Global.System.Resources.ResourceManager
+        Private Shared resourceMan As Global.System.Resources.ResourceManager
         
-        Private resourceCulture As Global.System.Globalization.CultureInfo
+        Private Shared resourceCulture As Global.System.Globalization.CultureInfo
+        
+        <Global.System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")>  _
+        Friend Sub New()
+            MyBase.New
+        End Sub
         
         '''<summary>
         '''  Returns the cached ResourceManager instance used by this class.
         '''</summary>
         <Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
-        Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
+        Friend Shared ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("multylang.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("multylang.MsgRes", GetType(MsgRes).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
@@ -51,7 +55,7 @@ Namespace My.Resources
         '''  resource lookups using this strongly typed resource class.
         '''</summary>
         <Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
-        Friend Property Culture() As Global.System.Globalization.CultureInfo
+        Friend Shared Property Culture() As Global.System.Globalization.CultureInfo
             Get
                 Return resourceCulture
             End Get
@@ -61,12 +65,12 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to Hello world.
+        '''  Looks up a localized string similar to hello world.
         '''</summary>
-        Friend ReadOnly Property MyStr() As String
+        Friend Shared ReadOnly Property MyStr() As String
             Get
                 Return ResourceManager.GetString("MyStr", resourceCulture)
             End Get
         End Property
-    End Module
+    End Class
 End Namespace
